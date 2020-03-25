59eb37ea6d2193c8e143505a4f701cbc7680e7da
Ben <curtben1@hotmail.co.uk>
2020-03-25 10:39:30 +0000

    some basic client updates and bug fixes,
    hopefully my ip dont change again

--- mainProgClient.py
+++ mainProgClient.py
@@ -2,21 +2,23 @@ import mainloop as mL
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-host = "86.173.43.185"   # ip of my home pc add this in later and maybe replace with pasberry pi                          
+host = "86.168.151.34"   # ip of my home pc add this in later and maybe replace with pasberry pi                          
 port = 7070 # port forward this on my router
 s.connect((host, port))                            
 
 def main():
-    menu=input("would you like to connect to the server or play a local hand (LOCAL/SERVER): ")
+    menu=input("would you like to connect to the server or play a local hand or view the server list(LOCAL/SERVER/VIEW): ")
     if menu=="LOCAL":
         table=mL.Table()
         table.playHand()
-    else:
+    elif menu=="HOST":
         uName=input("what is your username (no spaces)")        # A placeholder until actual usernames are implemented 
         uName=uName.encode("ascii")
         s.send(uName)                             
         msg = s.recv(1024)                                     
         s.close()
         print (msg.decode('ascii'))
+    elif menu == "VIEW":
+        
 
 main() 
