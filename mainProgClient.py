0a682e4ac4f075b1573321d058915f72d6d5453c
Ben <curtben1@hotmail.co.uk>
2020-04-05 14:41:09 +0100

    Update mainProgClient.py

--- mainProgClient.py
+++ mainProgClient.py
@@ -4,10 +4,11 @@ import pickle
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = "86.170.85.22"   # ip of my home pc add this in later and maybe replace with pasberry pi                          
-port = 7070 
+port = 7070 # port forward this on my router
 s.connect((host, port))                            
 
-def login(menu):                # may need to make each of these there there own function for when ui gets integrated
+def login():                # may need to make each of these there there own function for when ui gets integrated
+    menu=input("would you like to connect to the server or play a local hand or view the server list(LOCAL/SERVER/VIEW): ")
     if menu=="LOCAL":
         table=mL.Table()
         table.playHand()
@@ -26,19 +27,4 @@ def login(menu):                # may need to make each of these there there own
         msg=pickle.loads(msg)
     return msg
 
-
-def menu():
-    menu=input("would you like to connect to the server or play a local hand or view the server list(LOCAL/HOST/VIEW): ")
-    returned=login(menu)
-    if menu=="VIEW":
-        print("Server list is: \n",returned)
-        serverChoice=input("which server id do you want")
-        for i in range(len(returned)):
-            if returned[i][0] == serverChoice:
-                newip=returned[i][2]
-                break
-        s.connect((newip,8080))
-        uName=input("username: ")       #temp, once db structure+logons are setup it will read from there. This is for testing
-        request=uName
-        request = request.encode("ascii")
-        s.send
+print(login())
