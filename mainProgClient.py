c10bb4becfa7b017ab2b5f0f416daaa25bdaaa68
Ben <curtben1@hotmail.co.uk>
2020-04-05 14:37:56 +0100

    updated dev log and started actual join implemtation

--- mainProgClient.py
+++ mainProgClient.py
@@ -4,11 +4,10 @@ import pickle
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = "86.170.85.22"   # ip of my home pc add this in later and maybe replace with pasberry pi                          
-port = 7070 # port forward this on my router
+port = 7070 
 s.connect((host, port))                            
 
-def login():                # may need to make each of these there there own function for when ui gets integrated
-    menu=input("would you like to connect to the server or play a local hand or view the server list(LOCAL/SERVER/VIEW): ")
+def login(menu):                # may need to make each of these there there own function for when ui gets integrated
     if menu=="LOCAL":
         table=mL.Table()
         table.playHand()
@@ -27,4 +26,19 @@ def login():                # may need to make each of these there there own fun
         msg=pickle.loads(msg)
     return msg
 
-print(login())
+
+def menu():
+    menu=input("would you like to connect to the server or play a local hand or view the server list(LOCAL/HOST/VIEW): ")
+    returned=login(menu)
+    if menu=="VIEW":
+        print("Server list is: \n",returned)
+        serverChoice=input("which server id do you want")
+        for i in range(len(returned)):
+            if returned[i][0] == serverChoice:
+                newip=returned[i][2]
+                break
+        s.connect((newip,8080))
+        uName=input("username: ")       #temp, once db structure+logons are setup it will read from there. This is for testing
+        request=uName
+        request = request.encode("ascii")
+        s.send
