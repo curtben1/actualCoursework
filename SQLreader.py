713c7fd6a4063c5c5b63931a06b73ac8d7e1cfb7
curtben1 <60194144+curtben1@users.noreply.github.com>
2020-04-06 10:40:38 +0100

    Update SQLreader.py

--- SQLreader.py
+++ SQLreader.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
-##from cryptography.hazmat.backends import default_backend
-##from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 """
 Contains functions necessary for reading/writing to an sql database
